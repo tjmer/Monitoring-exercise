19f1eceb18a864e3e2712d48458def6d88270ed7
Tyler Merritt <tjmerritt5@gmail.com>
2021-08-31 14:46:55 -0600

    added a function

---
+++ public/index.js
@@ -0,0 +1,23 @@
+const form = document.querySelector('form')
+const addpeople = body => axios.post('http://localhost:4005/api/people', body).then(returnpeople)
+function submitHandler(e){
+    e.preventDefault()
+
+    const personName = document.querySelector('#name')
+    const personAge = document.querySelector('#age')
+    const favColor = document.querySelector('#favcolor')
+    
+    let bodyObj = {
+        name: personName.value,
+        age: personAge.value,
+        color: favColor.value
+    }
+
+    addpeople(bodyObj)
+
+     personName = ''
+     personAge = ''
+     favColor = ''
+}
+
+form.addEventListener('submit', submitHandler)
