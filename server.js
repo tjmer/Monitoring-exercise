777fae0de0ef150d6f20b2eeb816d60c0a473a0e
Tyler Merritt <tjmerritt5@gmail.com>
2021-08-31 12:31:18 -0600

    added rollback

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const path = require('path')
+const Rollbar = require('rollbar')
+
+let rollbar = new Rollbar({
+    accessToken: 'ee87f05c2dd546d29d6ddf201cbb5baa',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
 
 const app = express()
 const port = process.env.PORT || 4005
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file served successfully')
 })
-
+app.use(rollbar.errorHandler())
 app.listen(port, ()=> console.log(`Up on ${port}`))
