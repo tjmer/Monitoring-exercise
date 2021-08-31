9393eab957a8fa934e2cd48b53c2770973cb862d
Tyler Merritt <tjmerritt5@gmail.com>
2021-08-31 16:04:00 -0600

    added if statement

--- public/index.js
+++ public/index.js
@@ -1,3 +1,4 @@
+const Rollbar = require("rollbar")
 
 const form = document.querySelector('form')
 const preview = document.querySelector('#peoplePlace')
@@ -7,25 +8,29 @@ const getPeople = ()=> axios.get('/api/people').then(peopleReturn)
 const addpeople = body => axios.post('/api/people', body).then(peopleReturn)
 function submitHandler(e){
     e.preventDefault()
-
     const personName = document.querySelector('#name')
     const personAge = document.querySelector('#age')
     const favColor = document.querySelector('#favcolor')
-    
-    let bodyObj = {
-        name: personName.value,
-        age: personAge.value,
-        color: favColor.value
+    if (personName != '' && personAge != '' && favColor != ''){ 
+        let bodyObj = {
+            name: personName.value,
+            age: personAge.value,
+            color: favColor.value
+        }
+        
+        addpeople(bodyObj)
+        
+        personName.value = ''
+        personAge.value = ''
+        favColor.value = ''
+    }else if (personName === '' && personAge === '' && favColor === ''){
+        Rollbar.error("Missing name, age and color.")
+    }else if (personName === '' || personAge === "" || favColor === ""){
+        Rollbar.warning('missing info')
     }
-
-    addpeople(bodyObj)
-
-     personName.value = ''
-     personAge.value = ''
-     favColor.value = ''
-}
-
-function personInfo(group){
+    }
+    
+    function personInfo(group){
     const displayPlate = document.createElement('div')
     displayPlate.innerHTML = `<h4>${group.name}</h4><p>${group.age}</p><p>${group.color}`
 }
