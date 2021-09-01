9588aaf236d25fa947dae49f37155889652280d9
Tyler Merritt <tjmerritt5@gmail.com>
2021-08-31 21:43:05 -0600

    added a couple of alerts

--- public/index.js
+++ public/index.js
@@ -1,9 +1,10 @@
 const form = document.querySelector('form')
 const preview = document.querySelector('#peoplePlace')
 const peopleReturn = ({data: group}) => previewPerson(group)
-const getPeople = ()=> axios.get('/api/people').then(peopleReturn)
 
+const getPeople = ()=> axios.get('/api/people').then(peopleReturn)
 const addpeople = body => axios.post('/api/people', body).then(peopleReturn)
+
 function submitHandler(e){
     e.preventDefault()
     const personName = document.querySelector('#name')
@@ -22,9 +23,11 @@ function submitHandler(e){
         personAge.value = ''
         favColor.value = ''
     }else if (personName.value === '' && personAge.value === '' && favColor.value === ''){
+        alert('Missing name, age and color')
         console.log("Missing name, age and color.")
         Rollbar.critical("No info input");
     }else{
+        alert('Missing info')
         console.log('missing info')
         Rollbar.warning("One or more fields not filled")
     }
